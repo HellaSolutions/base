7013f07c5dc28e56d5eb316c4707aa3537a6afa0
Paola <paola.greco@hella-solutions.com>
2017-11-12 00:37:13 +0100

    Last commit MergeSort

--- maven.root/sorting/src/main/java/it/hella/sorting/MergeSort.java
+++ maven.root/sorting/src/main/java/it/hella/sorting/MergeSort.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SuppressWarnings("rawtypes")
 public class MergeSort<T extends Comparable> implements Sorter<T> {
+
+	private static final Logger logger = LoggerFactory.getLogger(MergeSort.class);
 
 	public void sort(List<T> list) {
 		if (list == null || list.isEmpty()) {
@@ -17,51 +22,51 @@ public class MergeSort<T extends Comparable> implements Sorter<T> {
 	@SuppressWarnings("unused")
 	private void sort(List<T> list, int a, int b) {
 
-		if (a < 0 || b > list.size()) {
-			throw new IllegalArgumentException("Illegal bounds: " + a + ", " + b);
+		if (a >= b - 1){
+			return;
 		}
-		int delta = (a - b) / 2;
-		while (delta > 1) {
-			sort(list, a, delta);
-			sort(list, delta + 1, b);
-			merge(list, a, delta, b);
-		}
+		int delta = (a + b - 1) / 2;
+		sort(list, a, delta + 1);
+		sort(list, delta + 1, b);
+		merge(list, a, delta, b);
 
 	}
 
 	@SuppressWarnings("unchecked")
 	void merge(List<T> list, int a, int delta, int b) {
 		List<T> temp = new ArrayList<>();
-		int j = a; 
+		int j = a;
 		int k = delta + 1;
-		while(true){
-			
-			while(j <= delta && list.get(j).compareTo(list.get(k)) <= 0){
+		if (k >= b)
+			return;
+		while (true) {
+
+			while (j <= delta && list.get(j).compareTo(list.get(k)) <= 0) {
 				temp.add(list.get(j));
 				j++;
 			}
-			if (j > delta){
-				for(;k < b; k++){
+			if (j > delta) {
+				for (; k < b; k++) {
 					temp.add(list.get(k));
 				}
 				break;
 			}
-			while(k < b && list.get(k).compareTo(list.get(j)) <= 0){
+			while (k < b && list.get(k).compareTo(list.get(j)) <= 0) {
 				temp.add(list.get(k));
 				k++;
 			}
-			if (k >= b){
-				for(;j <= delta; j++){
+			if (k >= b) {
+				for (; j <= delta; j++) {
 					temp.add(list.get(j));
 				}
 				break;
 			}
 		}
-		for(j = a; j < b; j++){
-			list.set(j, temp.get(j));
+		for (j = a; j < b; j++) {
+			list.set(j, temp.get(j - a));
 		}
 		temp.clear();
-	
+
 	}
 
 }
--- maven.root/sorting/src/test/java/it/hella/sorting/MergeSortTests.java
+++ maven.root/sorting/src/test/java/it/hella/sorting/MergeSortTests.java
@@ -1,23 +1,50 @@
 package it.hella.sorting;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import it.hella.sorting.MergeSort;
 
 public class MergeSortTests {
 	
+	private static final Logger logger = LoggerFactory.getLogger(MergeSortTests.class);
+	
+	private static final int MAX_TEST = 1000;
+	private static final int MAX_SIZE = 10000;
 	private MergeSort<Integer> sorter;
 
 	@Before
 	public void before(){
 		sorter = new MergeSort<Integer>();
+	}
+	
+	@Test
+	public void mergeTestOne(){
+		
+		List<Integer> list = Arrays.asList(10);
+		sorter.merge(list, 0, 0, 1);
+		assertThat(list, contains(10));
+		
+	}
+	
+	@Test
+	public void mergeTestCouple(){
+		
+		List<Integer> list = Arrays.asList(20, 10);
+		sorter.merge(list, 0, 0, 2);
+		assertThat(list, contains(10, 20));
+		
 	}
 	
 	@Test
@@ -38,12 +65,61 @@ public class MergeSortTests {
 		
 	}
 	
+	
+	@Test
+	public void mergeTestEqualsElements(){
+		
+		List<Integer> list = Arrays.asList(10, 20, 30, 40, 50, 11, 30, 31, 40, 51);
+		sorter.merge(list, 0, 4, 10);
+		assertThat(list, contains(10, 11, 20, 30, 30, 31, 40, 40, 50, 51));
+		
+	}
+	
 	@Test
 	public void mergeTestAlternate(){
+		
+		List<Integer> list = Arrays.asList(11, 21, 31, 41, 51, 10, 20, 30, 40, 50);
+		sorter.merge(list, 0, 4, 10);
+		assertThat(list, contains(10, 11, 20, 21, 30, 31, 40, 41, 50, 51));
+		
+	}
+	
+	@Test
+	public void mergeTestAlternateReversed(){
 		
 		List<Integer> list = Arrays.asList(10, 20, 30, 40, 50, 11, 21, 31, 41, 51);
 		sorter.merge(list, 0, 4, 10);
 		assertThat(list, contains(10, 11, 20, 21, 30, 31, 40, 41, 50, 51));
+		
+	}
+	
+	@Test
+	public void mergeSortTest(){
+		
+		List<Integer> list = Arrays.asList(3, 9, 6, 1, 4, 9, 3, 4, 3, 8);
+		sorter.sort(list);
+		assertThat(list, contains(1, 3, 3, 3, 4, 4, 6, 8, 9, 9));
+		
+	}
+	
+	@Test
+	public void mergeSortRandomTest(){
+		
+		List<Integer> random = new ArrayList<>();
+		Random rg = new Random();
+		for(int t = 0; t <= MAX_TEST; t++){
+			for (int i = 0; i < MAX_SIZE; i++){
+				random.add(rg.nextInt(MAX_SIZE));
+			}
+			logger.error(Arrays.toString(random.stream().toArray()));
+			sorter.sort(random);
+			logger.error(Arrays.toString(random.stream().toArray()));
+			for(int i = 0; i < MAX_SIZE - 1; i++){
+				assertTrue(random.get(i) <= random.get(i + 1));
+			}
+			random.clear();
+			
+		}
 		
 	}
 	
